34912b91b5b7832afc3583ca94cab96618534455
M.Gill <78278997+9mgill@users.noreply.github.com>
2022-07-27 18:49:13 -0400

    Add files via upload

--- Web Application/venv/Scripts/activate.bat
+++ Web Application/venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\ManjotSingh\My Drive (9mgill@gmail.com)\Final\web-app\venv
+set VIRTUAL_ENV=C:\Users\ManjotSingh\My Drive (9mgill@gmail.com)\Devp\Fake News Data\web-app\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
